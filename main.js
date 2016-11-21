c36bd81ecaccc23da8d9c89ca8eea5301da4b0a2
Isaac <r.sato7690@me.com>
2016-11-21 21:11:50 +0900

    Format code

--- main.js
+++ main.js
@@ -4,16 +4,31 @@ const {RtmClient} = client;
 const {RTM_EVENTS} = client;
 const RTM_CLIENT_EVENTS = client.CLIENT_EVENTS.RTM;
 
-const misdoKita = new RegExp("ミス(ター)?ド(ーナ(ッ)?ツ)?.*(来|き)た");
-
 const rtm = new RtmClient(config.token);
 rtm.start();
 
-rtm.on(RTM_EVENTS.MESSAGE, (m) => {
-    //対象チャンネル外、またはmisudoKitaに一致しない時にreturn
-    if(!m.text) var misdoKitaFlag = !m.text.match(misdoKita);
-    if(!m.text || m.channel != config.otsukaiChannel || misdoKitaFlag) return;
+var stage = 0;
+var master = ""; // :doughnut: を募集した人
 
-    console.log("Start otsukai!");
-    rtm.sendMessage(`お使いシーケンスを開始しました。 <@${m.user}> さん、待機時間を指定してください。1分以内に回答が無ければ、デフォルトの30分が適用されます。`, m.channel);
+rtm.on(RTM_EVENTS.MESSAGE, (m) => {
+    if(!m.text || m.channel != config.otsukaiChannel) return;
+
+    switch(stage) {
+    case 0:
+        let misdoKita = new RegExp("ミス(ター)?ド(ーナ(ッ)?ツ)?.*(来|き)た");
+        if(!m.text.match(misdoKita)) return;
+        console.log("Start otsukai!");
+        master = m.user;
+        rtm.sendMessage(`お使いシーケンスを開始しました。欲しい :doughnut: と個数をココに投げてください。 <@${master}> さんが「締め切り」と発言すると締め切ります。`, m.channel);
+        stage++;
+        break;
+    case 1:
+        if(m.user == master && m.text.match(/締め切り/)) {
+            rtm.sendMessage(`受付を締め切りました。結果は以下の通りです。では <@${master}> さん、よろしくお願いします！`, m.channel);
+            stage = 0;
+        } else {
+            console.log(m.text);
+        }
+        break;
+    }
 });
